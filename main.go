64a1de2c68cd42aeae67489ac24d71034753ebad
llotosl <lotosyt1@gmail.com>
2022-02-26 22:46:05 +0500

    Nothing

--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	requests "github.com/llotosl/manycalls2.0/pkg/requests"
 )
 
 func main() {
-	client := &http.Client{}
-	request := requests.NewRequest(client)
-	headers := map[string]string{}
-
-	data, contentType, err := requests.MakeBoundary("fdsQEFFJjffjgHkf", dataHead)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(data)
-	fmt.Println(contentType)
+	fmt.Println("s")
 }
